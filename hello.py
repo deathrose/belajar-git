e5c877136613b55be98e2e1e8b9a6db38006ea45
Mr Pixelheartz <pixelheartz@localhost.localdomain>
2019-10-06 16:55:43 +0700

    File tadi terlalu simple buat kamu

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print('hello, ini file python')
+
+
+kamu = "ganteng"
+if (kamu == "ganteng"):
+	print("Mantap gan")
+elif (kamu == "jelek"):
+	print("Maaf kamu terlalu baik")
+else:
+	print("error")
+
